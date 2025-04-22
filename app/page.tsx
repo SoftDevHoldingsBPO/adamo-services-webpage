3e01a29b43cc0c1c319e689b2a4d0236ce78b851
Nurpais Orozulbaev <nurpaiso@reqo.com>
2025-04-22 23:52:59 +0600

    Add icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { HamburgerMenuIcon } from "@/components/icon";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="container">
+      <Button>
+        <HamburgerMenuIcon />
+      </Button>
       <h1 className="heading-1">
         Automatización y optimización sin complicaciones
       </h1>
