299d2e0a89399152afc7b23e8518bc77f9dab903
Nurpais Orozulbaev <nurpaiso@reqo.com>
2025-06-01 04:56:47 +0600

    Small fixes

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,21 +1,25 @@
 "use client";
 
-import { BlogPost, BlogPostsResponse, getBlogPosts } from "@/services/blog";
+import { BlogPostsResponse, getBlogPosts } from "@/services/blog";
 import { useQuery } from "@tanstack/react-query";
 
 import BlogFilter from "@/components/sections/Blog/BlogFilter";
 import BlogGrid from "@/components/sections/Blog/BlogGrid";
 
 export default function Page() {
-  const { data } = useQuery<BlogPostsResponse>({
+  const { data, isLoading } = useQuery<BlogPostsResponse>({
     queryKey: ["blogPosts"],
     queryFn: getBlogPosts,
   });
 
+  if (isLoading) {
+    return <div className="text-center py-10">Loading...</div>;
+  }
+
   return (
-    <>
+    <div data-cursor-text="">
       <BlogFilter />
       <BlogGrid posts={data?.blogPosts ?? []} />
-    </>
+    </div>
   );
 }
