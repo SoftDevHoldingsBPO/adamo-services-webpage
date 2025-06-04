0380b337b91fc57bd335cf8bf17487379522d83c
Nurpais Orozulbaev <nurpaiso@reqo.com>
2025-06-04 08:57:52 +0600

    Fix navbar

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -1,18 +1,7 @@
-import { BlogProvider } from "@/providers/BlogProvider";
-
-import BlogNavbar from "@/components/layout/BlogNavbar";
-import Footer from "@/components/layout/Footer";
-
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <BlogProvider>
-      <BlogNavbar />
-      <main className="flex-auto">{children}</main>
-      <Footer />
-    </BlogProvider>
-  );
+  return <main className="flex-auto">{children}</main>;
 }
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -8,10 +8,13 @@ import { useState } from "react";
 
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { CloseIcon, HamburgerMenuIcon, Logo } from "../icon";
 import { Button } from "../ui/button";
 import LocaleSelect from "../ui/locale-select";
+import BlogNavbar from "./BlogNavbar";
+import Navigation from "./Navigation";
 
 const SCROLL_TOP_THRESHOLD = 64;
 
@@ -32,56 +35,75 @@ const Navbar = () => {
     }
   });
 
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/blog")) {
+    return (
+      <>
+        <BlogNavbar />
+        <Navigation />
+      </>
+    );
+  }
+
   return (
-    <div
-      data-animation-down="4"
-      data-fixed
-      data-at-top={isAtTop}
-      data-open={isOpen}
-      className="flex items-center justify-between px-4 py-6 lg:px-6 fixed top-0 inset-x-0 z-40 group bg-transparent md:bg-white data-[at-top=false]:bg-white data-[at-top=false]:shadow-sm data-[at-top=false]:py-3 data-[open=true]:bg-primary data-[open=true]:shadow-none data-[open=true]:py-6"
-      style={{
-        transition: "padding 350ms ease-out, background-color 100ms ease-out",
-      }}
-    >
-      <Link href="/">
-        <Logo className="text-white md:text-primary group-data-[at-top=false]:text-primary transition-colors group-data-[open=true]:text-white" />
-      </Link>
-      <div className="flex items-center gap-x-6">
-        <LocaleSelect
-          align={desktop ? "start" : "end"}
-          className="text-white active:text-neutral-100 group-data-[at-top=false]:text-neutral-600 group-data-[at-top=false]:hover:text-neutral-700 group-data-[at-top=false]:active:text-neutral-800 md:text-neutral-600 md:hover:text-neutral-700 md:active:text-neutral-800 group-data-[open=true]:!text-white"
-        />
+    <>
+      <div
+        data-animation-down="4"
+        data-fixed
+        data-at-top={isAtTop}
+        data-open={isOpen}
+        className="flex items-center justify-between px-4 py-6 lg:px-6 fixed top-0 inset-x-0 z-40 group bg-transparent md:bg-white data-[at-top=false]:bg-white data-[at-top=false]:shadow-sm data-[at-top=false]:py-3 data-[open=true]:bg-primary data-[open=true]:shadow-none data-[open=true]:py-6"
+        style={{
+          transition: "padding 350ms ease-out, background-color 100ms ease-out",
+        }}
+      >
+        <Link href="/">
+          <Logo className="text-white md:text-primary group-data-[at-top=false]:text-primary transition-colors group-data-[open=true]:text-white" />
+        </Link>
+        <div className="flex items-center gap-x-6">
+          <LocaleSelect
+            align={desktop ? "start" : "end"}
+            className="text-white active:text-neutral-100 group-data-[at-top=false]:text-neutral-600 group-data-[at-top=false]:hover:text-neutral-700 group-data-[at-top=false]:active:text-neutral-800 md:text-neutral-600 md:hover:text-neutral-700 md:active:text-neutral-800 group-data-[open=true]:!text-white"
+          />
 
-        {/* Mobile */}
-        <div className="md:hidden">
-          <Button
-            size="md"
-            onClick={toggleMenu}
-            variant={isOpen ? "secondary" : isAtTop ? "secondary" : "primary"}
-          >
-            {isOpen ? <CloseIcon /> : <HamburgerMenuIcon />}
-          </Button>
-        </div>
+          {/* Mobile */}
+          <div className="md:hidden">
+            <Button
+              size="md"
+              onClick={toggleMenu}
+              variant={isOpen ? "secondary" : isAtTop ? "secondary" : "primary"}
+            >
+              {isOpen ? <CloseIcon /> : <HamburgerMenuIcon />}
+            </Button>
+          </div>
 
-        {/* Desktop */}
-        <div className="hidden md:block">
-          <Button asChild size="md" variant={isOpen ? "secondary" : "primary"}>
-            <Link href="/adamo-pay">{t("contact")}</Link>
-          </Button>
-        </div>
+          {/* Desktop */}
+          <div className="hidden md:block">
+            <Button
+              asChild
+              size="md"
+              variant={isOpen ? "secondary" : "primary"}
+            >
+              <Link href="/adamo-pay">{t("contact")}</Link>
+            </Button>
+          </div>
 
-        {/* Desktop */}
-        <div className="hidden md:block">
-          <Button
-            size="md"
-            variant={isOpen ? "secondary" : "primary"}
-            onClick={toggleMenu}
-          >
-            {isOpen ? <CloseIcon /> : <HamburgerMenuIcon />}
-          </Button>
+          {/* Desktop */}
+          <div className="hidden md:block">
+            <Button
+              size="md"
+              variant={isOpen ? "secondary" : "primary"}
+              onClick={toggleMenu}
+            >
+              {isOpen ? <CloseIcon /> : <HamburgerMenuIcon />}
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Navigation />
+    </>
   );
 };
 
