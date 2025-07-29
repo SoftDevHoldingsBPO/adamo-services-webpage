52f1b3972e9fc9e9f85f2168d18c3b9e5542364f
Nurpais Orozulbaev <orozulbaev@gmail.com>
2025-07-29 15:58:12 +0600

    Fix tooltip

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -49,7 +49,7 @@ function TooltipContent({
         sideOffset={sideOffset}
         align={align}
         className={cn(
-          "bg-neutral-800 text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit max-w-[350px] origin-(--radix-tooltip-content-transform-origin) rounded-lg p-3 font-semibold text-[11px] leading-[18px] text-justify",
+          "bg-neutral-800 text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit max-w-[300px] origin-(--radix-tooltip-content-transform-origin) rounded-lg p-3 font-semibold text-[11px] leading-[18px] text-justify",
           className,
         )}
         {...props}
