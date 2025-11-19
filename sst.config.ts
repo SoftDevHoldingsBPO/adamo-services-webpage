42749e777f3c267fd09c936fdb7c9e3ffd2b7414
Alvaro <alvaro.chico@ocasystem.com>
2025-11-19 09:35:35 -0500

    Deploy Adamoservices with ctions

---
+++ sst.config.ts
@@ -0,0 +1,47 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "adamo-services",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      region: "us-east-2",
+    };
+  },
+  async run() {
+    const cloudfront = await import("aws-cdk-lib/aws-cloudfront");
+
+    const bucket = new sst.aws.Bucket("adamo-services-bucket", {
+      access: "public",
+    });
+
+    const isProd = $app.stage === "production";
+
+    new sst.aws.Nextjs("adamo-services-app", {
+      link: [bucket],
+      ...(isProd
+        ? {
+            customDomain: {
+              domainName: "adamoservices.co",
+              hostedZone: "adamoservices.co",
+            },
+          }
+        : {}),
+      server: {
+        environment: {
+          NEXT_PUBLIC_CLOUD_ENV: isProd ? "prod" : "dev",
+        },
+      },
+      cdk: {
+        distribution: {
+          defaultBehavior: {
+            cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+            originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
+          },
+        },
+      },
+    });
+  },
+});
